a2f26e94fc604365760277748d090c52bc97c992
Ivan Revelino <234983236+ivanrevelino@users.noreply.github.com>
2026-09-06 18:13:37 +0200

    created exception handler for methodArgumentNotValidException

--- src/main/java/com/ecommerce/ecommerce_api/exception/ExceptionDetails.java
+++ src/main/java/com/ecommerce/ecommerce_api/exception/ExceptionDetails.java
@@ -1,17 +1,18 @@
-package com.ecommerce.ecommerce_api.infra.handler;
+package com.ecommerce.ecommerce_api.exception;
 
-import lombok.Builder;
 import lombok.Data;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 
-@Builder
 @Data
-public class ErrorMessage {
-
+@SuperBuilder
+public class ExceptionDetails {
     private String message;
+    private String title;
     private int status;
     private String error;
+    private String details;
     private String developer_message;
     private LocalDateTime timestamp = LocalDateTime.now();
 }
--- src/main/java/com/ecommerce/ecommerce_api/infra/handler/RestExceptionHandler.java
+++ src/main/java/com/ecommerce/ecommerce_api/infra/handler/RestExceptionHandler.java
@@ -1,36 +1,58 @@
 package com.ecommerce.ecommerce_api.infra.handler;
 
-import com.ecommerce.ecommerce_api.exception.BadRequestException;
-import com.ecommerce.ecommerce_api.exception.InsufficientStockException;
-import com.ecommerce.ecommerce_api.exception.ResourceNotFoundException;
+import com.ecommerce.ecommerce_api.exception.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
+@Slf4j
 public class RestExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorMessage> resourceNotFoundException(ResourceNotFoundException exception) {
-        ErrorMessage errorMessage = ErrorMessage.builder()
+    public ResponseEntity<BadRequestExceptionDetails> resourceNotFoundException(ResourceNotFoundException exception) {
+        BadRequestExceptionDetails badRequestExceptionDetails = BadRequestExceptionDetails.builder()
                 .message(exception.getMessage())
                 .status(HttpStatus.NOT_FOUND.value())
                 .error(HttpStatus.NOT_FOUND.getReasonPhrase())
                 .developer_message("Se vira ai cara - Dev Ivan")
                 .build();
-        return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(badRequestExceptionDetails, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({BadRequestException.class, InsufficientStockException.class})
-    public ResponseEntity<ErrorMessage> handleBadRequest(Exception exception) {
-        ErrorMessage errorMessage = ErrorMessage.builder()
+    public ResponseEntity<BadRequestExceptionDetails> handleBadRequest(Exception exception) {
+        BadRequestExceptionDetails badRequestExceptionDetails = BadRequestExceptionDetails.builder()
                 .message(exception.getMessage())
                 .status(HttpStatus.BAD_REQUEST.value())
                 .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
                 .developer_message("Se vira ai cara - Dev Ivan")
                 .build();
-        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(badRequestExceptionDetails, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationExceptionDetails> methodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(","));
+        String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(","));
+
+        return new ResponseEntity<>(ValidationExceptionDetails.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .fields(fields)
+                .fieldsMessage(fieldsMessage)
+                .title("Bad Request Exception, Invalid Fields")
+                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .developer_message("Se vira ai cara - Dev Ivan")
+                .build()
+        , HttpStatus.BAD_REQUEST);
+    }
 }
